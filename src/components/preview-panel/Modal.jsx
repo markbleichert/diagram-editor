46e226304dfdf5c89071040357a5c129741775a3
Mark Bleichert <markbleichert@gmail.com>
2019-04-19 00:21:49 +0200

    add copy function to modal

--- src/components/preview-panel/Modal.jsx
+++ src/components/preview-panel/Modal.jsx
@@ -7,7 +7,7 @@ class DownloadModal extends React.Component {
 
         this.state = {
             open: false
-        }
+        };
     }
 
     onOpenModal() {
@@ -18,15 +18,37 @@ class DownloadModal extends React.Component {
         this.setState({ open: false });
     };
 
+    selectAll(e) {
+        this.textArea.select();
+
+        try {
+            const ok = document.execCommand('copy');
+
+            if (ok) {
+                this.message.innerHTML = 'Copied to clipboard !';
+            } else {
+                this.message.innerHTML = 'Unable to copy!';
+            }
+        } catch (err) {
+            this.message.innerHTML = 'Unsupported Browser!';
+        }
+    }
+
     render() {
         return (
             <span>
                 <div className="download" onClick={this.onOpenModal.bind(this)}></div>
                 <Modal open={this.state.open} onClose={this.onCloseModal.bind(this)} center>
-                    <h2>Model</h2>
-                    <p>
-                        <textarea className="qa-model" defaultValue={this.props.data}></textarea>
-                    </p>
+                    <div className="downlad-modal-content">
+                        <h2>Model</h2>
+                        <p>
+                            <textarea
+                                ref={(el) => {this.textArea = el}}
+                                className="qa-model" defaultValue={this.props.data}></textarea>
+                        </p>
+                        <span ref={(el) => {this.message = el}}></span>
+                        <button onClick={this.selectAll.bind(this)}>Copy</button>
+                    </div>
                 </Modal>
             </span>
         );
